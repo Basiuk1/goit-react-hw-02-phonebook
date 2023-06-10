7318b8200c0594b4099d553b63136976d44f65e8
Yura Basiuk <yurik.kamenev@gmail.com>
2023-06-11 00:11:53 +0300

    start task number 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Form from './Form';
+// import { nanoid } from 'nanoid';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+  // model.id = nanoid()
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.formSubmitHandler} />
+        <button type="submit">Add contact</button>
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
